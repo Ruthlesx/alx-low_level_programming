596179650321289fc2edf1e1c5b8cf7a2009424c
Ruthlesx <ifeoramicheal@gmail.com>
2023-08-16 15:53:58 -0700

    nested functions 6-11

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs- computer the absolute value of an integer
+ * @n the int too check
+ * Return. the absolute value of int 
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return(n);
+	}
+	return(-n);
+}
